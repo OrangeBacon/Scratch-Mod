054d4d83211c4044f203ad2d76d64f5e524d7174
ScratchOs <ScratchOs@users.noreply.github.com>
2015-06-13 09:42:26 +0100

    Create math.js

---
+++ src/math.js
@@ -0,0 +1,22 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.comment = function(string) {};
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            [' ', '/ %s', 'comment', 'comment'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Mod - Misc', descriptor, ext);
+})({});
