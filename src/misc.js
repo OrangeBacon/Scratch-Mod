12bf9fe1c5a6d3bad025403d7f574b0c6d6fddb9
ScratchOs <ScratchOs@users.noreply.github.com>
2015-06-15 17:44:19 +0100

    Update misc.js

--- src/misc.js
+++ src/misc.js
@@ -9,11 +9,21 @@
     };
 
     ext.comment = function(string) {};
+    
+    ext.title_r = function() {
+        return document.title;
+    };
+    
+    ext.title_s = function(string) {
+        document.title = string;
+    };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             [' ', '/ %s', 'comment', 'comment'],
+            ["r", "tab name", "title_r"],
+            [" ", "Set tab name to %s", "title_s", "Hello World"],
         ]
     };
 
