e84e84533aa00940a0f0e17b8da643dd534138e2
ScratchOs <ScratchOs@users.noreply.github.com>
2015-06-17 17:57:27 +0100

    Update installer.js

--- src/installer.js
+++ src/installer.js
@@ -1,3 +1,4 @@
 ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/misc.js');
 ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/math.js');
 ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/logic.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/html5local.js');
