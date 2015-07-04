3b5d515c227655750cc936455b6dcd4bf479fbba
ScratchOs <ScratchOs@users.noreply.github.com>
2015-07-04 16:13:24 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -8,10 +8,15 @@
         eval("\(function\(ext\) \{ext\._shutdown \= function\(\) \{\}; ext\._getStatus \= function\(\) \{return \{status\: 2, msg\: \'Ready\'\};\}; ext\." + name + " \= function\(" + params + "\) \{" + js + "\}; var descriptor \= \{blocks: \[\[\'" + type + "\', \'" + desc + "'\, \'" + name + "\'\]\,\]\,\};ScratchExtensions\.register\(\'" + name + "\'\, descriptor\, ext\)\;\}\)\(\{\}\)\;")
     };
     
+    ext.jar = function(script) {
+        eval(script);
+    }
+    
     var descriptor = {
         blocks: [
-            [' ', 'create block: name %s params %s js %s type %s desc %s', 'create', "custom", "r", "block"],
-            ],
+            [' ', 'create block: name %s params %s js %s type %s desc %s', 'create', "custom", "hello", "return true", "r", "true %s"],
+            [' ', 'eval %s', 'jar', 'return true'],
+        ],
     };
 
     ScratchExtensions.register('Mod - Custom', descriptor, ext);
