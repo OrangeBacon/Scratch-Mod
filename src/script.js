984d24ad23d7cb00ad20bf50563d2eff646bb763
ScratchOs <ScratchOs@users.noreply.github.com>
2015-07-04 10:23:38 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,10 +4,14 @@
         return {status: 2, msg: 'Ready'};
     };
     
-    ext.create = function()
+    ext.create = function(name, params, js, type, desc) {
+        eval("(function(ext) {ext._shutdown = function() {}; ext._getStatus = function() {return {status: 2, msg: 'Ready'};}; ext." + name + " = function(" + params + ") {" + js + "}; var descriptor = {blocks: [[\'" + type + "\', \'" + desc + "'\, \'" + name + "\'],],};ScratchExtensions.register(\'" + name + "\', descriptor, ext);})({});")
+    };
+    
     var descriptor = {
-        blocks: [],
-        menus: {}
+        blocks: [
+            [' ', 'create block: name %s params %s js %s type %s desc %s', 'create', "custom", "r", "block"],
+            ],
     };
 
     ScratchExtensions.register('Mod - Custom', descriptor, ext);
