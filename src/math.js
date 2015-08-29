04bc649383e6faa7e1e72d166097326dab0b323e
ScratchOs <ScratchOs@users.noreply.github.com>
2015-08-29 16:21:05 +0100

    Update math.js

--- src/math.js
+++ src/math.js
@@ -28,6 +28,26 @@
     ext.counter_a = function(no) {
         counter = counter + no;
     };
+    
+    ext.math = function(constant) {
+        if (constant == "e") {
+            return Math.E;
+        } else if (constant == "pi") {
+            return Math.PI;
+        } else if (constant == "sqrt 2") {
+            return Math.SQRT2;
+        } else if (constant == "sqrt 1/2") {
+            return Math.SQRT1_2;
+        } else if (constant == "natural logarithm of 2") {
+            return Math.LN2;
+        } else if (constant == "natural logarithm of 10") {
+            return Math.LN10;
+        } else if (constant == "base 2 logarithm of E") {
+            return Math.LOG2E;
+        } else if (constant == "base 10 logarithm of E") {
+            return Math.LOG10E;
+        };
+    };
 
     // Block and block menu descriptions
     var descriptor = {
@@ -36,8 +56,12 @@
             ["r", "%n √ %n", "root", 3, 8],
             ["r", "counter", "counter_r"],
             [" ", "reset counter", "counter_s"],
-            [" ", "change counter by %n", "counter_a", 1]
-        ]
+            [" ", "change counter by %n", "counter_a", 1],
+            ["r", "Constant %m.math", "math", "pi"],
+        ],
+        menus: {
+            math: ["e", "pi", "sqrt 2", "sqrt 1/2", "natural logarithm of 2", "natural logarithm of 10", "base 2 logarithm of E", "base 10 logarithm of E"]
+        }
     };
 
     // Register the extension
