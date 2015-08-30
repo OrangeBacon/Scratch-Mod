23b607f5cb39058eee6f6ca51c0d53464976a313
ScratchOs <ScratchOs@users.noreply.github.com>
2015-08-30 11:33:20 +0100

    Update math.js

--- src/math.js
+++ src/math.js
@@ -48,7 +48,23 @@
             return Math.LOG10E;
         };
     };
-
+    
+    ext.colour = function(a,r,g,b) {
+        var ra;
+        var rr;
+        var rg;
+        var rb;
+        if (a>255) {ra = 255} else if (a<1) {ra = 1} else {ra = a};
+        if (r>255) {rr = 255} else if (r<0) {rr = 0} else {rr = r};
+        if (g>255) {rg = 255} else if (g<0) {rg = 0} else {rg = g};
+        if (b>255) {rb = 255} else if (b<0) {rb = 0} else {rb = b};
+        console.log(ra);
+        console.log(rr);
+        console.log(rg);
+        console.log(rb);
+        return (Math.floor(ra)*256)+(Math.floor(rr)*256)+(Math.floor(rg)*256)+(Math.floor(rb)*256)
+    };
+    
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
@@ -58,6 +74,7 @@
             [" ", "reset counter", "counter_s"],
             [" ", "change counter by %n", "counter_a", 1],
             ["r", "Constant %m.math", "math", "pi"],
+            ["r", "A:%n R:%n G:%n B:%n", "colour", 255, 255, 255, 255]
         ],
         menus: {
             math: ["e", "pi", "sqrt 2", "sqrt 1/2", "natural logarithm of 2", "natural logarithm of 10", "base 2 logarithm of E", "base 10 logarithm of E"]
