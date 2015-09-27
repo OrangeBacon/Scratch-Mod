4dddb21024fe4ad40ea8f1e45efa3a09f70dda85
ScratchOs <ScratchOs@users.noreply.github.com>
2015-09-27 11:31:19 +0100

    Optimisations
    removed obsolete blocks, page background.

--- src/misc.js
+++ src/misc.js
@@ -1,42 +1,25 @@
 (function(ext) {
-    // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
 
     ext.comment = function(string) {};
     
-    ext.title_r = function() {
+    ext.title = function() {
         return document.title;
     };
     
-    ext.title_s = function(string) {
+    ext.title_set = function(string) {
         document.title = string;
     };
     
-    ext.page_back_r = function () {
-        return document.body.style.background;
-    };
-    
-    ext.page_back_s = function (url) {
-        return document.body.style.background = "url(" + url + ") no-repeat right top";
-    };
-    
-    // Block and block menu descriptions
     var descriptor = {
         blocks: [
             [' ', '/ %s', 'comment', 'comment'],
-            ["r", "tab name", "title_r"],
-            [" ", "Set tab name to %s", "title_s", "Hello World"],
-            ["r", "page background", "page_back_r"],
-            [" ", "Set page background to %s", "page_back_s", "url"],
+            ["r", "tab name", "title"],
+            [" ", "Set tab name to %s", "title_set", "Hello World"],
         ]
     };
-
-    // Register the extension
     ScratchExtensions.register('Mod - Misc', descriptor, ext);
 })({});
